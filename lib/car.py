f64c7b17b83e11812b959709b51e1aa20dfe4b02
+ <jacqueline.trapp18@gmail.com>
2024-12-12 12:12:22 -0500

    lab complete

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,11 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+accord = Car(30, 4)
+print(accord.go())
+print(accord.wheel_size)
